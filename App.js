3b463692b93318bfa114ac90034e13e108daf772
Mona Waseem <monaqwaseem@gmail.com>
2023-03-06 16:03:05 -0500

    changing root to footer only

--- App.js
+++ App.js
@@ -19,56 +19,9 @@ const HeaderComponent = () => {
   );
 };
 
-// Functional Component Example B: (varible --> Title must be capatalize)
-// to render using tag: root.render(<HeaderComponent />);
-const Title = () => (
-  <h1 id="title" key="h2">
-    Namaste React
-  </h1>
-);
-
-// 2 ways:
-// 1.B: no curly braces, --> Title must be capatalize & <Title />> must be in < /tag> format
-// 2.B: you can call Title as a function {Title()}
-//Example 1.B: <Title />> must be in < /tag> format:
-const HeaderComponent1 = () => {
-  return (
-    <div>
-      <Title />
-      <h2>Namaste React Functional Component with Title Tag 🤪</h2>
-      <h2>This is a h2 tag</h2>
-    </div>
-  );
-};
-
-//2.B: you can call Title as a function {Title()}
-const HeaderComponent2 = () => {
-  return (
-    <div>
-      {Title(console.log("Hello World"))}
-      <h2>Namaste React Functional Component with Title function 🤪</h2>
-      <h2>This is a h2 tag</h2>
-    </div>
-  );
-};
-
-// WRITING COMMENTS INSIDE JSX?
-
-const HeaderComponent3 = () => {
-  return (
-    <div>
-      {
-        // This is a comment.
-        /**
-         *
-         *
-         * This is a multi line comment.
-         *
-         */
-      }
-    </div>
-  );
-};
+// CAN YOU JUST USE REACT IN YOUR FOOTER ONLY? YES! --> By your "root".
+// Change the root to "footer" and it will only render in the footer.
+// AND In index.html change the id="root" to id="footer"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
