fbd42d06fdc02319e7d6045eed8cdf0e089b0c87
Mona Waseem <monaqwaseem@gmail.com>
2023-03-07 14:12:17 -0500

    Build the Header, Title and NavItems Components.

--- App.js
+++ App.js
@@ -1,32 +1,9 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 
-// // React Element Example C: Third Way
-const Title = () => (
-  <h1 id="title" key="h2">
-    Food Villa App
-  </h1>
-);
-
-
-const HeaderComponent = () => {
-  return (
-    <div>
-      <Title />
-      <h2>Namaste React - Most commonly Used Syntax</h2>
-    </div>
-  );
-};
-
-// Build the layout.
-// Step 1. Create your AppLayout, list it out. 
-
-const AppLayout = () => {
-  return (
-    { 
-      /**
+/**
     Header
-      - Logo
+      - Title/Logo
       - Nav Items (Right Side)
       - Cart
     Body 
@@ -41,19 +18,39 @@ const AppLayout = () => {
     Footer
      - links
      - copyrights
-  */ 
- 
-    }
+  */
+
+ // Step 1. Create your AppLayout, list it out.
+const AppLayout = () => {
+  return {};
+};
+
+// Step 2. Build the Header and Header Components.
+// What are the components in the Header? Title/Logo, NavItems, Cart..
+// Step 2.a. Create the Title Component.
+// Step 2.b. Create the NavItems Component.
+
+const Title = () => (
+  <h1 id="title" key="h2">
+    Food Villa App
+  </h1>
+);
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
   );
-
-
-
-
-
-
-
-
-
+};
 
 
 
